fe6635c91aa84eb0a21ef365157205a71140fe0e
Syed Hussaini <syedahmedhussaini@gmail.com>
2011-06-01 10:53:34 -0400

    Initial Refinery - SASS Enabled

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nulogyrefinery::Application.config.secret_token = '8c197fc22acf5676a7fd1b1800433969a468347bde05f6994932859ed311c31266c1d130820f0d2eb658e0d7091a2ee891f276fbfe3b8ef3f742399a313e770c'
